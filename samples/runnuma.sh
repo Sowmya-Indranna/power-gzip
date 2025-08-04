7721f2aa9f23f7cdc88426cde16b9d7f442e6c52
Sowmya-Indranna <131340936+Sowmya-Indranna@users.noreply.github.com>
2025-08-04 17:05:57 +0530

    filter out NUMA nodes that have no CPUs assigned
    As in issue created for the error mentioned in https://github.com/libnxz/power-gzip/issues/211 -  runnuma.sh fails with numa_sched_setaffinity_v2_int() failed: Invalid argument if nodes doesn't contain any cpu's assigned.

--- samples/runnuma.sh
+++ samples/runnuma.sh
@@ -1,39 +1,38 @@
 #!/bin/bash
 
-#filename and number of instances
+# filename and number of instances
 echo $1 $2
 
 C=$2
 
 rm -f *.nx.gunzip
 
-for S in `seq 1 $C` 
+for S in `seq 1 $C`
 do
     FN=$S.$1
-	echo $FN
-	rm -f $FN
+    echo $FN
+    rm -f $FN
 
-	# create the unique file
+    # create the unique file
     cp $1 $FN
 
-	# FS cache warmup
+    # FS cache warmup
     cat $FN > /dev/null
 done
 
-nodes=`numastat | head -1`
+# Get NUMA nodes that actually have CPUs
+NODES=$(numactl --hardware | grep "node [0-9]" | grep cpus: | grep -v "cpus:$" | awk '{print $2}')
+
 for S in `seq 1 $C`
 do
-	FN=$S.$1    
-	for i in $nodes
-	do
-		node=`echo $i | sed -e "s/node//g"`
-		if [ "$node" -lt "16" ]; then
-			numactl -N $node ./gunzip_nx_test $FN  &
-		fi
-	done
+    FN=$S.$1
+    for node in $NODES
+    do
+        if [ "$node" -lt "16" ]; then
+            echo "Running on NUMA node $node: ./gunzip_nx_test $FN"
+            numactl -N $node ./gunzip_nx_test $FN  &
+        fi
+    done
 done
 
 wait
-
-
- 
